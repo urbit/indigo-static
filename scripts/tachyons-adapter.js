369279da4fac8c0d7ff8fc7ef1df2cfc451d5985
Nick Beattie <nick@nickbytes.com>
2019-09-09 21:37:15 -0700

    feature: spacing (#3)
    * add spacing
    
    * chore: generated

--- scripts/tachyons-adapter.js
+++ scripts/tachyons-adapter.js
@@ -17,10 +17,26 @@ const themeAdapter = async () => {
     lineHeight.push(theme.lineHeights[heightKey]);
   });
 
+  let spacing = [];
+
   return {
     lineHeight,
     customMedia: [{ m: 550 / 16 }, { l: 750 / 16 }, { xl: 960 / 16 }],
     colors,
+    spacing: [
+      4 / 16,
+      8 / 16,
+      12 / 16,
+      16 / 16,
+      20 / 16,
+      24 / 16,
+      32 / 16,
+      48 / 16,
+      64 / 16,
+      96 / 16,
+      160 / 16,
+      288 / 16
+    ],
     typeScale: [4.5, 4, 3, 2, 1.5, 1.125, 1, 0.875, 0.75]
   };
 };
